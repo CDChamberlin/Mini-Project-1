1bc2cb6fc90130adfd56c37f29bbf3a4eff2a419
Collin <cdchamberlin.rva@gmail.com>
2023-12-01 12:22:02 -0500

    Carousel is working. Condensed JS code. Broke filter...(). Problem most likely being that there's a problem in the array format.

--- Express/myapp/README.md
+++ Express/myapp/README.md
@@ -1,6 +1,11 @@
 # Mini-Project-1
 A Fake E-Commerce Website for my first Mini Project of my Software Engineering Class.
 
+## Design
+    [Hand Sketches](placeholder)
+    [Figma](https://www.figma.com/file/HoFuE1FQm5WEqNzgT75vdh/MIni-Project-1?type=design&node-id=1%3A1395&mode=design&t=AKh0LXDu3mhfXjGD-1)
+
+
 ## Features
 - Landing Page: Main Page of the website. Allows the user to browse, but putting something into the cart goes to login Page 
 - Login Page: Allows the user to Login or Create an account
--- Express/myapp/public/js/script.js
+++ Express/myapp/public/js/script.js
@@ -1,34 +1,67 @@
 let productList = [];
-let loggedIn = false
-const mainCard = loggedIn ? "small-card" : "featured-card"
+let cart = [];
+let cards = [];
+let loggedIn = false;
+const mainCard = loggedIn ? "featured-card" : "small-card";
 
-const myCarousel = document.querySelector('#carouselExample')
-const carousel = new bootstrap.Carousel(myCarousel) 
+const myCarousel = document.querySelector("#carouselFeatured");
+const carousel = new bootstrap.Carousel(myCarousel);
 
-function addCard(img, title, price, description, id) {
-  console.log(id)
-  const template = document
-    .getElementById(id)
-    .content.cloneNode(true);
-  // populate the template
-  if (isValidHttpUrl(img)) {
-    template.getElementById("small-card-img").src = img;
-    template.getElementById("small-card-img").alt = description;
-  }
-  template.querySelector(".card-title").innerText = title;
-  template.querySelector(".card-subtitle").innerText = `$ ${price.toFixed(2)}`;
-  template.querySelector(".card-text").innerText = description;
-  document.querySelector("#card-list").appendChild(template);
-}
+// fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((json) => {
+//     productList = json;
+//     // Display the data using the createCard function
+//     productList.forEach((element) => {
+//       createCard(element, mainCard);
+//     });
+//     // Call featuredCard after the data is fetched
+//     featuredCard();
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
 
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((json) => {
-    json.forEach((element) => {
-      addCard(element.image, element.title, element.price, element.description, mainCard);
-      productList.push(element);
+    productList = json;
+    // Display the data using the createCard function
+    productList.forEach((element) => {
+      createCard(element, mainCard);
     });
+    // Call featuredCard after the data is fetched
+    featuredCard();
+    displayCards(cards);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
+
+function createCard(item, id) {
+  const template = document.getElementById(id).content.cloneNode(true);
+  // populate the template
+  if (isValidHttpUrl(item.image)) {
+    template.getElementById(id + "-img").src = item.image;
+    template.getElementById(id + "-img").alt = item.description;
+  }
+  template.querySelector(".card-title").innerText = item.title;
+  template.querySelector(".card-subtitle").innerText = `$ ${item.price.toFixed(
+    2
+  )}`;
+  template.querySelector(".card-text").innerText = item.description;
+  cards.push(template);
+  return template;
+}
+function displayCards(cardArray) {
+  cardArray.forEach((card) =>
+    document.querySelector("#card-list").append(card)
+  );
+}
+
+function loadCards(item) {
+  return createCard(item, "featured-card");
+}
 
 function isValidHttpUrl(string) {
   try {
@@ -38,29 +71,38 @@ function isValidHttpUrl(string) {
     return false;
   }
 }
-console.log(productList);
-function filterCatagories(event) {
-  console.log("Hi")
+
+function filterCategories(event) {
   const list = document.getElementById("card-list");
-  const selected = event; // Gets the selections from the dropdown menu.
+  const selected = event;
+  let updated = [];
   while (list.hasChildNodes()) list.removeChild(list.firstChild);
 
   if (selected === "all Catagories") {
-    productList.forEach((element) => {
-      addCard(element.image, element.title, element.price, element.description, mainCard);
-    });
+    updated = Array.from(cards);
   } else {
-    let updated = productList.filter((item) => {
+    updated = productList.filter((item) => {
       return item.category === selected;
     });
-    updated.forEach((element) => {
-      addCard(element.image, element.title, element.price, element.description);
-    });
+  }
+  displayCards(updated);
+}
+
+function featuredCard() {
+  let numbers = [
+    Math.floor(Math.random() * 7),
+    Math.floor(Math.random() * (13 - 8) + 8),
+    Math.floor(Math.random() * (19 - 14) + 14),
+  ];
+  for (let i = 0; i < 3; i++) {
+    document
+      .getElementById("carousel-item" + i)
+      .append(loadCards(productList[numbers[i]]));
   }
 }
 
-function featuredCard(){
-  let numbers = [Math.floor(Math.random()*7),Math.floor(Math.random()*(13-8)+8), Math.floor(Math.random()*(19-14)+14)]
-  //numbers.forEach(item =>  )
-
+function addToCart() {
+  console.log("Item added.");
+  cart.push(this);
+  console.log(cart);
 }
